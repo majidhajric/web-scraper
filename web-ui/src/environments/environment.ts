83330f72cde918f2d93602177d1fcc3d4e500a12
majid <majidhajric@gmail.com>
2021-03-31 20:11:58 +0200

    CircleCI config change

--- web-ui/src/environments/environment.ts
+++ web-ui/src/environments/environment.ts
@@ -1,19 +1,6 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
   production: false,
-  apiServer: 'http://localhost:8080',
-  authServer: 'http://localhost:9090/auth/realms/scraper',
+  apiServer: 'https://demo-web-scraper-service.herokuapp.com',
+  authServer: 'https://dev-authentication-server.herokuapp.com/auth/realms/scraper',
   authClientId: 'scraper'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
